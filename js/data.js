9b8093605106099ab79a23eda0a278da93bc2bcc
user-PC\user <lu29743524@yahoo.com>
2022-08-13 12:20:59 +0800

    圖片路徑修正

--- js/data.js
+++ js/data.js
@@ -3,105 +3,105 @@
 const iceItemsData = [
     {
        id: 'iceItem1',
-       img: '../material/chocolate.jpg',
+       img: 'material/chocolate.jpg',
        name: '黑巧克力碎焦糖咖啡冰淇淋',
        price: 120,
        description:'1'
     },
     {
         id: 'iceItem2',
-        img: '../material/sesame.jpg',
+        img: 'material/sesame.jpg',
         name: '榛果黑芝麻冰淇淋',
         price: 100,
         description:'2' 
     },
     {
         id: 'iceItem3',
-        img: '../material/vanilla.jpg',
+        img: 'material/vanilla.jpg',
         name: '香草可可冰淇淋',
         price: 90,
         description:'3' 
     },
     {
         id: 'iceItem4',
-        img: '../material/bubble-tea-ice.jpg',
+        img: 'material/bubble-tea-ice.jpg',
         name: '珍珠奶茶冰淇淋',
         price: 70,
         description:'4' 
     },
     {
         id: 'iceItem5',
-        img: '../material/mango-milk.jpg',
+        img: 'material/mango-milk.jpg',
         name: '芒果牛奶冰淇淋',
         price: 150,
         description:'5' 
     },
     {
         id: 'iceItem6',
-        img: '../material/blueberry-cheese.jpg',
+        img: 'material/blueberry-cheese.jpg',
         name: '藍莓起士蛋糕冰淇淋',
         price: 80,
         description:'6' 
     },
     {
         id: 'iceItem7',
-        img: '../material/banana.jpg',
+        img: 'material/banana.jpg',
         name: '香蕉冰淇淋',
         price: 50,
         description:'7' 
     },
     {
         id: 'iceItem8',
-        img: '../material/cappuccino.jpg',
+        img: 'material/cappuccino.jpg',
         name: '卡布奇諾冰淇淋',
         price: 200,
         description:'8' 
     },
     {
         id: 'iceItem9',
-        img: '../material/tiramisu.jpg',
+        img: 'material/tiramisu.jpg',
         name: '提拉米蘇冰淇淋',
         price: 180,
         description:'9' 
     },
     {
         id: 'iceItem10',
-        img: '../material/lemon.jpg',
+        img: 'material/lemon.jpg',
         name: '檸檬雪花冰',
         price: 80,
         description:'10' 
     },
     {
         id: 'iceItem11',
-        img: '../material/mango.jpg',
+        img: 'material/mango.jpg',
         name: '愛文芒果冰',
         price: 300,
         description:'11' 
     },
     {
         id: 'iceItem12',
-        img: '../material/red-been.jpg',
+        img: 'material/red-been.jpg',
         name: '紅豆麻糬冰',
         price: 70,
         description:'12' 
     },
     {
         id: 'iceItem13',
-        img: '../material/red-bean-matcha.jpg',
+        img: 'material/red-bean-matcha.jpg',
         name: '紅豆抹茶冰',
         price: 70,
         description:'13' 
     },
     {
         id: 'iceItem14',
-        img: '../material/red-coffee.jpg',
+        img: 'material/red-coffee.jpg',
         name: '咖啡紅茶冰',
         price: 80,
         description:'14' 
     },
     {
         id: 'iceItem15',
-        img: '../material/strawberry-snow.jpg',
+        img: 'material/strawberry-snow.jpg',
         name: '草莓優格雪花冰',
         price: 80,
         description:'15' 
