9bdb8c74098fa72174d354d20bc277c529c848dc
user-PC\user <lu29743524@yahoo.com>
2022-08-13 18:25:25 +0800

    商品詳細頁js

--- js/product.js
+++ js/product.js
@@ -1,19 +1,17 @@
 let icecream = document.querySelector("#show_icecream .grid-container");
 let cut_ice = document.querySelector("#show_ice .container .row");
-let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateIce = () => {
     //============= 冰淇淋 =============//
     icecream.innerHTML = iceItemsData.slice(0, 9) //商品項目1~9
         .map((x, index) => {
             let { id, name, img } = x;
-            let search = basket.find((x) => x.id === id) || [];
             let dir = parseInt(index / 3) == 1 ? 'left' : 'right'; //商品項目除3餘1的AOS動畫向左跑，其餘AOS動畫向右跑
             return `
-            <div id=product-id-${id} class="grid_item" data-aos="zoom-in-${dir}">
+            <div id=${id} class="grid_item" data-aos="zoom-in-${dir}" onclick = "saveImg(${index})">
                 <p>${name}</p>
                 <a href="product_details/product_details.html"> 
-                    <img src=${img} alt="" onclick="saveImg(${id})">
+                    <img src="${img}" alt="">
                 </a>
             </div>
             `;
@@ -22,37 +20,24 @@ let generateIce = () => {
 
     //============= 剉冰 =============//
     cut_ice.innerHTML = iceItemsData.slice(9) //商品項目9以後，不包括9
-        .map((x) => {
+        .map((x, index) => {
             let { id, name, img } = x;
-            let search = basket.find((x) => x.id === id) || [];
             return `
-            <div id=product-id-${id} class="pic col-12 col-sm-6 col-md-4">
+            <div id=${id} class="pic col-12 col-sm-6 col-md-4" onclick = "saveImg(${index + 9})">
                 <p>${name}</p>
                 <a href="product_details/product_details.html">
-                    <img src=${img} alt="" onclick="saveImg(${id})">
+                    <img src="${img}" alt="">
                 </a>
             </div>
             `;
         })
-        .join(""); //去除逗號
+        .join("");
+
 };
 
 generateIce();
 
-//============= 點擊圖片時，把資料存到localstorage =============//
-let saveImg = (id) => {
-    // alert(1);
-    let selectedItem = id; //點擊加號時，取得每項物品的id
-    let search = basket.find((x) => x.id === selectedItem.id); //點擊的商品數量等於每項物品的id
-
-    if (search === undefined) {
-        basket.push({
-            id: selectedItem.id,
-            item: 1,
-        });
-    } else {
-        search.item += 1;
-    }
-    update(selectedItem.id);
-    localStorage.setItem("data", JSON.stringify(basket));
+//============= 點擊圖片時，把陣列位置存到localstorage =============//
+let saveImg = (e) => {
+    localStorage.setItem("index", JSON.stringify(e));
 };
--- product_details/product_details.js
+++ product_details/product_details.js
@@ -1,3 +1,42 @@
 "use strict";
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+let ind = JSON.parse(localStorage.getItem("index"));
+// console.log(ind, iceItemsData[ind]);
+
+
+//============= 把資料放在網頁上 =============//
+let setImg = () => {
+    let { id, name, img, description, price} = iceItemsData[ind];
+    let obj = document.querySelector(".container");
+    obj.innerHTML = `
+                <div class="left">
+                    <img src="../${img}" alt="" width="300px">
+                </div>
+
+                <div class="right">
+                    <h1>${name}</h1>
+                    <p>NT$ ${price}</p>
+                    <article>
+                        <p>${description}</p>
+                    </article>
+
+                    <form action="#" method="#">
+                        <div class="item_block">
+                            <p>購買數量:</p>
+                            <div class="shop_block">
+                                <i class="fa-solid fa-minus"></i>
+                                <input type="text" value="1" min="1">
+                                <i class="fa-solid fa-plus"></i>
+                            </div>
+                        </div>
+                        <div class="item_block">
+                            <a href="../shop.html">
+                                <button type="button">加入購物車</button>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+                `;
+}
+
+setImg();
